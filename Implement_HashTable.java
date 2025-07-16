5e1e2938ce4595294ad991a31b3fda87df0998fe
Leo Irakliotis <lgreco@gmail.com>
2025-07-16 16:11:36 +0000

    prep w8

--- Implement_HashTable.java
+++ Implement_HashTable.java
@@ -1,11 +1,10 @@
 public class Implement_HashTable {
     public static void main(String[] args) {
         // HashTable<String> demo = new HashTable<>();
-        nfs_HashTable demo = new nfs_HashTable<>();
+        nfs_HashTable<String> demo = new nfs_HashTable<>();
         demo.add("Alena");
         demo.add("William");
         demo.add("Bryanna");
-        System.out.println(demo);
         demo.add("Laji");
         demo.add("Savonna");
         demo.add("Kamaria");
@@ -15,5 +14,22 @@ public class Implement_HashTable {
         demo.add("Meet");
         demo.add("Ryan");
         demo.add("Noor");
+        demo.add("Leo");
+        demo.add("Milo");
+        demo.add("Baji");
+        demo.add("Shadow");
+        demo.add("Miracle");
+        demo.add("Pluto");
+        demo.add("Salem");
+        demo.add("Mina");
+        demo.add("Eric");
+        demo.add("Don");
+        demo.add("Avi");
+        demo.add("Chicago");
+        demo.add("Aurora");
+        demo.add("Ypsilanti");
+        demo.add("Madison");
+        demo.add("Milwaukee");
+        System.out.println(demo);
     }
 }
